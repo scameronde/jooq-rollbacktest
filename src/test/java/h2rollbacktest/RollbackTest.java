2b9305012d327ca8a27cb1d152a7d68d267be363
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2015-10-15 16:22:26 +0200

    initial

---
+++ src/test/java/h2rollbacktest/RollbackTest.java
@@ -0,0 +1,88 @@
+package h2rollbacktest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.jooq.Record;
+import org.jooq.SQLDialect;
+import org.jooq.TransactionalRunnable;
+import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RollbackTest {
+
+  @Before
+  public void setup() {
+    setupDB();
+  }
+
+  @After
+  public void cleanup() {
+    cleanupDB();
+  }
+
+  @Test
+  public void testDelete() {
+    withTransaction(configuration -> {
+      DSL.using(configuration).query("insert into TEST (ID) values (1)").execute();
+    });
+    withTransaction(configuration -> {
+      DSL.using(configuration).query("delete from TEST where ID=1").execute();
+    });
+  }
+
+  @Test
+  public void testDeleteAfterRollback() {
+    withTransaction(configuration -> {
+      DSL.using(configuration).query("insert into TEST (ID) values (1)").execute();
+    });
+    withTransaction(configuration -> {
+      DSL.using(configuration).query("insert into TEST (ID) values (1)").execute();
+    });
+  }
+
+  private void setupDB() {
+    withTransaction(configuration -> {
+      DSL.using(configuration).createTable("TEST").column("ID", SQLDataType.BIGINT.nullable(false)).execute();
+      DSL.using(configuration).alterTable("TEST").add(DSL.constraint("PK").primaryKey("ID")).execute();
+    });
+  }
+
+  private void cleanupDB() {
+    withTransaction(configuration -> {
+      DSL.using(configuration).dropTable("TEST").execute();
+    });
+  }
+
+  private Connection get() {
+    String url = "jdbc:h2:mem:runtime;DB_CLOSE_DELAY=-1";
+    Properties props = new Properties();
+
+    try {
+      Connection connection = DriverManager.getConnection(url, props);
+      connection.setAutoCommit(false);
+      return connection;
+    }
+    catch (SQLException e) {
+      throw new DataAccessException("no connection possible", e);
+    }
+  }
+
+  private void withTransaction(TransactionalRunnable transactionalCode) {
+    try (Connection connection = get()) {
+      DSL.using(connection, SQLDialect.H2).transaction(transactionalCode);
+    }
+    catch (SQLException e) {
+      throw new DataAccessException("no transaction possible", e);
+    }
+  }
+}
